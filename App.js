f6b85d73d1c8ac455172ff14f54b739423f4812d
DAB0mB <emanor6@gmail.com>
2019-09-06 18:52:46 +0300

    Add Gifted Chat mock

--- App.js
+++ App.js
@@ -1,38 +1,262 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { AppLoading, Asset, Linking } from 'expo'
+import React, { Component } from 'react'
+import { Platform, KeyboardAvoidingView, StyleSheet, View, Text } from 'react-native'
+import { Bubble, GiftedChat, SystemMessage } from 'react-native-gifted-chat'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
+import earlierMessages from './__mocks__/earlierMessages'
+import messagesData from './__mocks__/messages'
+import AccessoryBar from './components/AccessoryBar'
+import CustomActions from './components/CustomActions'
+import CustomView from './components/CustomView'
+import NavBar from './components/NavBar'
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  container: { flex: 1 },
+})
+
+const filterBotMessages = message =>
+  !message.system && message.user && message.user._id && message.user._id === 2
+const findStep = step => message => message._id === step
+
+const user = {
+  _id: 1,
+  name: 'Developer',
+}
+
+const otherUser = {
+  _id: 2,
+  name: 'React Native',
+  avatar: 'https://facebook.github.io/react/img/logo_og.png',
+}
+
+const KeyboardView = ({ children }) => (
+  Platform.OS === 'android' ? (
+    <KeyboardAvoidingView behavior="padding">
+       {children}
+    </KeyboardAvoidingView>
+  ) : (
+    <React.Fragment>
+      {children}
+    </React.Fragment>
+  )
+);
+
+export default class App extends Component {
+  state = {
+    step: 0,
+    messages: [],
+    loadEarlier: true,
+    typingText: null,
+    isLoadingEarlier: false,
+  }
+
+  _isMounted = false
+
+  componentDidMount() {
+    this._isMounted = true
+    // init with only system messages
+    this.setState({
+      messages: messagesData.filter(message => message.system),
+      appIsReady: true,
+    })
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false
+  }
+
+  onLoadEarlier = () => {
+    this.setState(previousState => {
+      return {
+        isLoadingEarlier: true,
+      }
+    })
+
+    setTimeout(() => {
+      if (this._isMounted === true) {
+        this.setState(previousState => {
+          return {
+            messages: GiftedChat.prepend(
+              previousState.messages,
+              earlierMessages,
+            ),
+            loadEarlier: false,
+            isLoadingEarlier: false,
+          }
+        })
+      }
+    }, 1000) // simulating network
+  }
+
+  onSend = (messages = []) => {
+    const step = this.state.step + 1
+    this.setState(previousState => {
+      const sentMessages = [{ ...messages[0], sent: true, received: true }]
+      return {
+        messages: GiftedChat.append(previousState.messages, sentMessages),
+        step,
+      }
+    })
+    // for demo purpose
+    setTimeout(() => this.botSend(step), Math.round(Math.random() * 1000))
+  }
+
+  botSend = (step = 0) => {
+    const newMessage = messagesData
+      .reverse()
+      // .filter(filterBotMessages)
+      .find(findStep(step))
+    if (newMessage) {
+      this.setState(previousState => ({
+        messages: GiftedChat.append(previousState.messages, newMessage),
+      }))
+    }
+  }
+
+  parsePatterns = linkStyle => {
+    return [
+      {
+        pattern: /#(\w+)/,
+        style: { textDecorationLine: 'underline', color: 'darkorange' },
+        onPress: () => Linking.openURL('http://gifted.chat'),
+      },
+    ]
+  }
+
+  renderCustomView(props) {
+    return <CustomView {...props} />
+  }
+
+  onReceive = text => {
+    this.setState(previousState => {
+      return {
+        messages: GiftedChat.append(previousState.messages, {
+          _id: Math.round(Math.random() * 1000000),
+          text,
+          createdAt: new Date(),
+          user: otherUser,
+        }),
+      }
+    })
+  }
+
+  onSendFromUser = (messages = []) => {
+    const createdAt = new Date()
+    const messagesToUpload = messages.map(message => ({
+      ...message,
+      user,
+      createdAt,
+      _id: Math.round(Math.random() * 1000000),
+    }))
+    this.onSend(messagesToUpload)
+  }
+
+  renderAccessory = () => <AccessoryBar onSend={this.onSendFromUser} />
+
+  renderCustomActions = props => {
+    return <CustomActions {...props} onSend={this.onSendFromUser} />
+  }
+
+  renderBubble = props => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          left: {
+            backgroundColor: '#f0f0f0',
+          },
+        }}
+      />
+    )
+  }
+
+  renderSystemMessage = props => {
+    return (
+      <SystemMessage
+        {...props}
+        containerStyle={{
+          marginBottom: 15,
+        }}
+        textStyle={{
+          fontSize: 14,
+        }}
+      />
+    )
+  }
+
+  renderFooter = props => {
+    if (this.state.typingText) {
+      return (
+        <View style={styles.footerContainer}>
+          <Text style={styles.footerText}>{this.state.typingText}</Text>
+        </View>
+      )
+    }
+    return null
+  }
+
+  onQuickReply = replies => {
+    console.log({ replies })
+    const createdAt = new Date()
+    if (replies.length === 1) {
+      this.onSend([
+        {
+          createdAt,
+          _id: Math.round(Math.random() * 1000000),
+          text: replies[0].title,
+          user,
+        },
+      ])
+    } else if (replies.length > 1) {
+      this.onSend([
+        {
+          createdAt,
+          _id: Math.round(Math.random() * 1000000),
+          text: replies.map(reply => reply.title).join(', '),
+          user,
+        },
+      ])
+    } else {
+      console.warn('replies param is not set correctly')
+    }
+  }
+
+  renderQuickReplySend = () => <Text>{' custom send =>'}</Text>
+
+  render() {
+    if (!this.state.appIsReady) {
+      return <AppLoading />
+    }
+    return (
+      <View
+        style={styles.container}
+        accessible
+        accessibilityLabel='main'
+        testID='main'
+      >
+        <NavBar />
+        <KeyboardView>
+          <GiftedChat
+            messages={this.state.messages}
+            onSend={this.onSend}
+            loadEarlier={this.state.loadEarlier}
+            onLoadEarlier={this.onLoadEarlier}
+            isLoadingEarlier={this.state.isLoadingEarlier}
+            parsePatterns={this.parsePatterns}
+            user={user}
+            onQuickReply={this.onQuickReply}
+            keyboardShouldPersistTaps='never'
+            renderAccessory={this.renderAccessory}
+            renderActions={this.renderCustomActions}
+            renderBubble={this.renderBubble}
+            renderSystemMessage={this.renderSystemMessage}
+            renderCustomView={this.renderCustomView}
+            renderFooter={this.renderFooter}
+            quickReplyStyle={{ borderRadius: 2 }}
+            renderQuickReplySend={this.renderQuickReplySend}
+          />
+        </KeyboardView>
+      </View>
+    )
+  }
+}
